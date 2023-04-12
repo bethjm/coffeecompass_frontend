5b4370d8075e2c8fcef219baf3a4f4edbefc1a1e
BethCries <b.jeanne.mills@gmail.com>
2023-04-12 17:50:16 -0400

    testing commits are working

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+const getCafes = () => {
+ axios
+   .get('http://localhost:8000/api/shops')
+   .then(
+     (response) => setCafe(response.data),
+     (err) => console.error(err)
+   )
+   .catch((error) => console.error(error))
+}
+
+
+  let [cafe, setCafe] = useState([])
+
+useEffect(() => {
+ getCafes()
+}, [])
+
+  return (
+    <>
+      <h1>Meow</h1>
+    </>
+  )
+}
+
+export default App
