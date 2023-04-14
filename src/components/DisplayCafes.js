60b3d0616a97691b4062c61e88bc00eafb07307a
BethCries <b.jeanne.mills@gmail.com>
2023-04-13 20:38:17 -0400

    got edit working

--- src/components/DisplayCafes.js
+++ src/components/DisplayCafes.js
@@ -14,7 +14,7 @@ const DisplayCafes = (props) => {
           <div key={cafes.id}>
             <h4>Name: {cafes.name}</h4>
             <h5>Phone: {cafes.phone}</h5>
-            <EditCafe handleUpdateCafe={props.handleUpdateCafe}/>
+            <EditCafe handleUpdateCafe={props.handleUpdateCafe} cafes={cafes}/>
             <button onClick={props.handleDeleteCafe} value={cafes.id}>
        X
      </button>
